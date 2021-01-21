9b38ecc3c51ac82381914f7d630a2ef7492508b6
Carol <carolquiterio@yahoo.com.br>
2021-01-21 10:47:29 -0300

    adding lint tools
    font: https://medium.com/swlh/what-is-android-lint-17fa0d87abb2
    
    all lint conmmands: http://tools.android.com/tips/lint-checks

--- helloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ helloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.helloworld
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,6 +9,7 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
